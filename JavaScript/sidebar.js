e161913d8d00317a0fb81eb0d970289b6c6d6692
ramanlohar <ramanlohar11@gmail.com>
2023-12-09 13:51:56 +0530

    //main
    //main

--- JavaScript/sidebar.js
+++ JavaScript/sidebar.js
@@ -45,12 +45,12 @@ sidebardiv.innerHTML = `
 
 function openNav() {
   document.getElementById("sidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+//   document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("sidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+//   document.getElementById("main").style.marginLeft = "0";
 }
 
 function showFilterOptions() {
